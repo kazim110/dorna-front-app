38a90cd9b7e13c5b43d953d5574291210383eb3c
kazim110 <kazim.mohammadi110@gmail.com>
2024-12-03 14:20:50 +0430

    fix color in section four

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -250,36 +250,36 @@ const Home = () => {
       <div className="absolute h-full top-0 left-0 w-[50%] bg-customYellow">
       </div>
       <div
-            className="absolute top-0 right-auto left-[50%] w-36 h-36 border-4 border-green-300 bg-cover bg-center rounded-full animate-customAnimationIn"
+            className="absolute top-10 right-auto left-[54%] w-80 h-80 border-4 border-customYellow bg-cover bg-center rounded-full animate-customAnimationIn"
             style={{
               backgroundImage: `url(${womenImage})`,
               animationDelay: "750ms",
             }}
           ></div>
       <div className="relative w-[100%] h-[100%] flex flex-col items-center justify-center">
-        <div className="absolute h-full flex flex-col justify-center items-center -translate-x-1/4">
+        <div className="absolute h-full top-12 flex flex-col justify-center items-center -translate-x-1/4">
           <div
-              className="w-80 h-80 bg-cover bg-center rounded-full -translate-x-3/4"
+              className="w-80 h-80 bg-cover bg-center rounded-full -translate-x-3/4 shadow-xl"
               style={{
                 backgroundImage: `url(${womenImage})`,
               }}
             ></div>
             <div
-              className="w-[432px] h-[432px] bg-cover bg-center rounded-full -translate-y-1/4"
+              className="w-[432px] h-[432px] bg-cover bg-center rounded-full -translate-y-1/4 shadow-xl"
               style={{
                 backgroundImage: `url(${womenImage})`,
                 animationDelay: "500ms",
               }}
             ></div>
             <div
-              className="w-80 h-80 bg-cover bg-center rounded-full  -translate-y-2/3 -translate-x-2/3"
+              className="w-80 h-80 bg-cover bg-center rounded-full  -translate-y-2/3 -translate-x-2/3 shadow-xl"
               style={{
                 backgroundImage: `url(${womenImage})`,
                 animationDelay: "250ms",
               }}
             ></div>
         </div>
-        <div className="absolute z-10 bg-black text-white rounded-lg p-6 shadow-lg max-w-lg text-center translate-x-1/4 translate-y-1/2">
+        <div className="absolute z-10 bg-black text-white rounded-lg p-6 shadow-lg max-w-lg text-center translate-x-1/4 translate-y-64">
           <h2 className="text-3xl font-bold mb-4">Our Team</h2>
           <p className="text-gray-300 mb-4">
             Nunc mi ipsum faucibus vitae aliquet. Pellentesque elit eget gravida
@@ -288,7 +288,7 @@ const Home = () => {
           </p>
           <a
             href="#"
-            className="inline-block px-6 py-2 bg-green-300 text-black rounded-lg hover:bg-green-400 transition"
+            className="inline-block px-6 py-2 bg-customYellow text-black rounded-lg hover:bg-green-400 transition"
           >
             Read More
           </a>
