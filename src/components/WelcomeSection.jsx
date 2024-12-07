58d4008ed162895b9854ee5bbfe3a16d01b5b5f0
kazim110 <kazim.mohammadi110@gmail.com>
2024-12-07 13:58:22 +0430

    fix font size in welcome section

--- src/components/WelcomeSection.jsx
+++ src/components/WelcomeSection.jsx
@@ -10,8 +10,8 @@ const WelcomeSection = () => {
           className="bg-cover bg-center h-[1080px] flex items-center justify-center flex-col"
           style={{ backgroundImage: `url(${backgroundImage})` }}
         >
-          <h2 className="lg:text-6xl md:text-2xl font-medium text-customGreen relative">Get the website</h2>
-          <h1 className="w-[80%] lg:text-8xl md:text-6xl uppercase text-customGreen font-bold">Of your dreams</h1>
+          <h2 className="text-xl lg:text-6xl font-medium text-customGreen relative">Get the website</h2>
+          <h1 className="w-[80%] lg:text-8xl text-6xl uppercase text-customGreen font-bold">Of your dreams</h1>
           <p className="w-[50%] text-xl text-customGreen mx-20 mt-3 mb-10">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
