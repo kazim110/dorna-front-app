bda1128d75ce5ac5843997720290b3752f3261e5
kazim110 <kazim.mohammadi110@gmail.com>
2025-01-21 19:33:21 +0430

    adjust partnerships images

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,12 @@ const Footer = () => (
   <footer className="bg-black text-white py-8">
   <div className="flex flex-col justify-center items-center max-w-screen-xl mx-auto text-center">
     <h4 className='text-4xl font-bold uppercase'>Partnerships</h4>
-    <img className='w-[40%]' src={reflexLogo} alt="Reflex Creative Group" />
-    <img className='w-[40%]' src={avanLogo} alt="Reflex Creative Group" />
-    <img className='w-[40%]' src={parsaLogo} alt="Reflex Creative Group" />
-    <img className='w-[40%]' src={safiLogo} alt="Reflex Creative Group" />
+    <div className='sm-text-xl flex flex-row m-4'>
+      <img className='w-[40%]' src={reflexLogo} alt="Reflex Creative Group" />
+      <img className='w-[40%]' src={avanLogo} alt="Reflex Creative Group" />
+      <img className='w-[40%]' src={parsaLogo} alt="Reflex Creative Group" />
+      <img className='w-[40%]' src={safiLogo} alt="Reflex Creative Group" />
+    </div>
   </div>
 </footer>
   );
