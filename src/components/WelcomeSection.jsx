8dc0530961288389c6b74a07202fc55772515fd2
kazim110 <kazim.mohammadi110@gmail.com>
2025-01-29 19:10:03 +0430

    change text and background from welcome section

--- src/components/WelcomeSection.jsx
+++ src/components/WelcomeSection.jsx
@@ -8,14 +8,13 @@ const WelcomeSection = () => {
         <section id="welcome" className="bg-blue-500 text-white text-center">
         <div
           className="bg-cover bg-center h-[1080px] flex items-center justify-center flex-col"
-          style={{ backgroundImage: `url(${backgroundImage})` }}
+          // style={{ backgroundImage: `url(${backgroundImage})` }}
         >
-          <h2 className="text-xl lg:text-6xl font-medium text-customGreen relative">Get the website</h2>
-          <h1 className="w-[80%] lg:text-8xl text-6xl uppercase text-customGreen font-bold">Of your dreams</h1>
+          <h1 className="w-[80%] lg:text-8xl text-6xl uppercase text-customGreen font-bold">Dorna Media</h1>
+          <h2 className="text-xl lg:text-6xl font-medium text-customGreen relative">On Creativity Line</h2>
           <p className="w-[50%] text-xl text-customGreen mx-20 mt-3 mb-10">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud.
+          We transform your vision into impactful advertising solutions. With a team of professionals and
+           creative thinkers, we deliver tailored strategies to connect your brand with its audience.
           </p>
           <a
           href="#contact" // Link to the contact section
